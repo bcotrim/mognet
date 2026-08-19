darjss/t3code

fix(ssh): surface a failed remote t3 install instead of a silent 0-byte server.log (#5132)

packages/ssh/src/tunnel.ts
@@ -426,10 +426,26 @@ fi
 if command -v t3 >/dev/null 2>&1; then
   exec t3 "$@"
 fi
+# npm extracts a package before it runs the native builds of its dependencies,
+# so a failed build (t3 depends on node-pty, which needs a C toolchain) leaves
+# the npx cache without a t3 executable. \`npx --yes\` then exits 0 without
+# running anything at all, which the caller only ever sees as a server that
+# never becomes ready. Resolve the CLI once up front so that install failure is
+# reported here, with npm's own output on stderr.
+require_installed_t3_cli() {
+  T3_CLI_PATH="$("$@" -- sh -c 'command -v t3' || true)"
+  if [ -n "$T3_CLI_PATH" ]; then
+    return 0
+  fi
+  printf 'Remote host installed %s but npm produced no t3 executable, which usually means a native dependency (node-pty) failed to build. Install a C toolchain on the remote host (Debian/Ubuntu: build-essential, Fedora/RHEL: gcc-c++ make, macOS: xcode-select --install) and try again.\\n' @@T3_PACKAGE_SPEC@@ >&2
+  return 1
+}
 if command -v npx >/dev/null 2>&1; then
+  require_installed_t3_cli npx --yes --package @@T3_PACKAGE_SPEC@@ || exit 1
   exec npx --yes @@T3_PACKAGE_SPEC@@ "$@"
 fi
 if command -v npm >/dev/null 2>&1; then
+  require_installed_t3_cli npm exec --yes --package @@T3_PACKAGE_SPEC@@ || exit 1
   exec npm exec --yes @@T3_PACKAGE_SPEC@@ -- "$@"
 fi
 printf 'Remote host is missing the t3 CLI and could not install @@T3_PACKAGE_SPEC@@ because node/npm/npx are unavailable on PATH. Install Node or configure a supported version manager for non-interactive shells.\\n' >&2
@@ -581,7 +597,11 @@ if [ -z "$REMOTE_PORT" ]; then
   printf 'managed\\n' >"$MANAGED_FILE"
   if ! wait_ready "@@T3_READY_TIMEOUT_MS@@"; then
     printf 'Remote T3 server did not become ready on 127.0.0.1:%s.\\n' "$REMOTE_PORT" >&2
-    tail -n 80 "$LOG_FILE" >&2 2>/dev/null || true
+    if [ -s "$LOG_FILE" ]; then
+      tail -n 80 "$LOG_FILE" >&2 2>/dev/null || true
+    else
+      printf 'It wrote nothing to %s, so it exited before producing any output.\\n' "$LOG_FILE" >&2
+    fi
     kill "$REMOTE_PID" 2>/dev/null || true
     wait_for_pid_exit "$REMOTE_PID"
     rm -f "$PID_FILE" "$PORT_FILE" "$MANAGED_FILE"

bcotrim/mognet

(cherry picked from commit 9d0f2fc212858290e79710b44c0635a28dd92c2f)

packages/ssh/src/tunnel.ts
@@ -426,10 +426,26 @@ fi
 if command -v t3 >/dev/null 2>&1; then
   exec t3 "$@"
 fi
+# npm extracts a package before it runs the native builds of its dependencies,
+# so a failed build (t3 depends on node-pty, which needs a C toolchain) leaves
+# the npx cache without a t3 executable. \`npx --yes\` then exits 0 without
+# running anything at all, which the caller only ever sees as a server that
+# never becomes ready. Resolve the CLI once up front so that install failure is
+# reported here, with npm's own output on stderr.
+require_installed_t3_cli() {
+  T3_CLI_PATH="$("$@" -- sh -c 'command -v t3' || true)"
+  if [ -n "$T3_CLI_PATH" ]; then
+    return 0
+  fi
+  printf 'Remote host installed %s but npm produced no t3 executable, which usually means a native dependency (node-pty) failed to build. Install a C toolchain on the remote host (Debian/Ubuntu: build-essential, Fedora/RHEL: gcc-c++ make, macOS: xcode-select --install) and try again.\\n' @@MOGNET_PACKAGE_SPEC@@ >&2
+  return 1
+}
 if command -v npx >/dev/null 2>&1; then
+  require_installed_t3_cli npx --yes --package @@MOGNET_PACKAGE_SPEC@@ || exit 1
   exec npx --yes @@MOGNET_PACKAGE_SPEC@@ "$@"
 fi
 if command -v npm >/dev/null 2>&1; then
+  require_installed_t3_cli npm exec --yes --package @@MOGNET_PACKAGE_SPEC@@ || exit 1
   exec npm exec --yes @@MOGNET_PACKAGE_SPEC@@ -- "$@"
 fi
 printf 'Remote host is missing the t3 CLI and could not install @@MOGNET_PACKAGE_SPEC@@ because node/npm/npx are unavailable on PATH. Install Node or configure a supported version manager for non-interactive shells.\\n' >&2
@@ -581,7 +597,11 @@ if [ -z "$REMOTE_PORT" ]; then
   printf 'managed\\n' >"$MANAGED_FILE"
   if ! wait_ready "@@MOGNET_READY_TIMEOUT_MS@@"; then
     printf 'Remote Mognet server did not become ready on 127.0.0.1:%s.\\n' "$REMOTE_PORT" >&2
-    tail -n 80 "$LOG_FILE" >&2 2>/dev/null || true
+    if [ -s "$LOG_FILE" ]; then
+      tail -n 80 "$LOG_FILE" >&2 2>/dev/null || true
+    else
+      printf 'It wrote nothing to %s, so it exited before producing any output.\\n' "$LOG_FILE" >&2
+    fi
     kill "$REMOTE_PID" 2>/dev/null || true
     wait_for_pid_exit "$REMOTE_PID"
     rm -f "$PID_FILE" "$PORT_FILE" "$MANAGED_FILE"